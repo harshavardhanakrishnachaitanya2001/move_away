95c16077c9041028abc8516e61f1e431e249614e
G. H. Krishna Chaitanya <g.h.krishnachaitanya@Gs-Mac-mini.local>
2023-05-06 16:58:30 +0530

    Astroids are falling correctly

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,71 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import toySpaceship from "../imags/toy_space_ship.png";
 import { motion, useMotionValue } from "framer-motion";
 import astroids from "../imags/astroids.png";
 
-
-const astroidVariants = {
-  initial:{
-    y:-1000,
-    x:Math.random()*window.innerWidth,
-  },
-  animate:{
-    y:1000,
-    transition:{
-      duration:3,
-      delay:2,
-      repeat:Infinity,
-      repeatDuration:3
-    },
-  }
-}
-
-const buttonVariants = {
-  click: {
-    scale: 0.9,
-  },
-};
-const spaceshipVariants = {
+const asteroidVariants = {
   animate: {
-    y: [2, -2, 2, -2, 2, -2, 2, -2, 2, -2, 2],
+    y: "100vh",
     transition: {
-      y: {
-        duration: 2,
-        repeat: Infinity,
-        repeatDuration: 1,
-        ease: "easeInOut",
-      },
+      duration: 5,
+      ease: "linear",
     },
   },
 };
 
-const Game = ({ score, setScore, setGameComplete }) => {
+const Game = ({setScore, setGameComplete }) => {
   const x = useMotionValue(0);
+  const [rocks, setRocks] = useState([]);
+
+  const addRock = () => {
+    const newRock = {
+      id: Date.now(),
+      x: Math.random() * window.innerWidth,
+      y: -50,
+    };
+    setRocks((prevRocks) => [...prevRocks, newRock]);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const numRocks = Math.floor(Math.random() * 5) + 1;
+      for (let i = 0; i < numRocks; i++) {
+        addRock();
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleMouseMove = (event) => {
+    x.set(event.clientX);
+  };
+
+  const handleFireClick = () => {
+    setScore((prevScore) => prevScore + 2);
+  };
+
   const restartGame = () => {
     setGameComplete(false);
     setScore(0);
   };
-  const handleMouseMove = (event) => {
-    x.set(event.clientX);
-  };
-  const handleFireClick = () => {
-    setScore(prevScore => prevScore + 2);
-  };
-  return (
-  <div className="h-screen">
-    {/* Game */}
 
-      {/* Astroid field */}
-      <div className="flex flex-wrap">
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        <motion.img variants={astroidVariants} initial="initial" animate="animate" src={astroids} alt="" className="h-40 w-30"/>
-        
-      </div>
+  return (
+    <div className="h-screen">
+      {/* Asteroid field */}
+      {rocks.map((rock) => (
+        <motion.img
+          key={rock.id}
+          variants={asteroidVariants}
+          animate="animate"
+          src={astroids}
+          alt=""
+          className="absolute"
+          style={{ left: rock.x, top: rock.y, width: 40, height: 40 }}
+        />
+      ))}
 
       {/* Spaceship */}
       <motion.div
@@ -77,23 +74,24 @@ const Game = ({ score, setScore, setGameComplete }) => {
           style={{ x }}
           src={toySpaceship}
           alt=""
-          variants={spaceshipVariants}
-          animate="animate"
+          animate={{ y: [2, -2, 2, -2, 2, -2, 2, -2, 2, -2, 2] }}
+          transition={{ y: { duration: 2, repeat: Infinity, repeatDuration: 1, ease: "easeInOut" } }}
           className="w-14 h-14"
         />
       </motion.div>
+
       {/* Restart button */}
       <div className="justify-center flex items-end fixed bottom-4 w-11/12">
         <motion.button
-          variants={buttonVariants}
+          variants={{ click: { scale: 0.9 } }}
           onClick={restartGame}
           whileTap="click"
-          className=" leading-relaxed italic bg-red-400 border rounded-lg p-2 text-xl text-white font-bold mx-auto w-32 "
+          className="leading-relaxed italic bg-red-400 border rounded-lg p-2 text-xl text-white font-bold mx-auto w-32 "
         >
           RESTART
         </motion.button>
       </div>
-  </div>
+    </div>
   );
 };
 
